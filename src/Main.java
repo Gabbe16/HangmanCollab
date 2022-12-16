b572dc387fe22684a397f93877a423ae46ce2ee0
Gligon3 <94972508+Gligon3@users.noreply.github.com>
2022-12-16 11:05:55 +0100

    jag har gjort klart med att he in alla ord som ska finnas i denna hängagubbe

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Player!");
 
-    }
-
-    public static <Hiddenword> void String(Hiddenword){
-
-    }
-
-    {
 
     }
 }
--- src/Ogabogawilliam.java
+++ src/Ogabogawilliam.java
@@ -1,5 +1,25 @@
-public class Ogabogawilliam {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Ogabogawilliam {
+    public static void main (String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("ord.txt"));
+
+        int slumptal = (int)(Math.random()*156)*2;
+
+        for (int i = 0 ; i < slumptal ; i++) {
+            scanner.nextLine();
+        }
+
+        System.out.println(scanner.nextLine()); {
+
+        }
     }
-}
+        public static String Gamecheck(String y) {
+
+            return y;
+
+        }
+    }
