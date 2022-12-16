74a099524b0ded67effd9fd611104906191463ae
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2022-12-16 11:08:39 +0100

    Counting characters är klar, påbörjan av gissningar.

--- src/GabrielMetoder.java
+++ src/GabrielMetoder.java
@@ -1,8 +1,21 @@
 public class GabrielMetoder {
     public static void main(String[] args) {
+        // String name = "Gabriel"; (ordet som ska bli hemligt)
+        // String secret = countChar(name); (gör ordet till underscores)
+        // System.out.println(secret); (printa ut alla underscores)
 
     }
-    public static int countChar(String x) {
+    public static String countChar(String x) {
+        String maskedWord = "";
+        int i = 0;
+        for (i = 0 ; i < x.length() ; i++) {
+            maskedWord += "_";
+        }
+        return maskedWord;
+    }
+    public static String Guesses() {
+        String[] alfabet = {};
+        String[] guesses = {};
         return;
     }
 }
