c1e6e17c6222b33bcc3c4e96e565e2f944dbc488
Gligon3 <94972508+Gligon3@users.noreply.github.com>
2022-12-20 15:11:07 +0100

    Har inte gjort så mycket idag.

--- src/Ogabogawilliam.java
+++ src/Ogabogawilliam.java
@@ -1,25 +1,30 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Ogabogawilliam {
-    public static void main (String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(new File("ord.txt"));
 
-        int slumptal = (int)(Math.random()*156)*3;
+        int slumptal = (int) (Math.random() * 156) * 2;
 
-        for (int i = 0 ; i < slumptal ; i++) {
+        for (int i = 0; i < slumptal; i++) {
             scanner.nextLine();
         }
 
-        System.out.println(scanner.nextLine()); {
+        System.out.println(scanner.nextLine());
+        {
 
         }
     }
-        public static String Gamecheck(String y) {
 
-            return y;
+    public static String Gamecheck(String y) {
 
-        }
+        String correctWord ;
+        String _a____B_ ;
+
+        return y;
     }
+}
