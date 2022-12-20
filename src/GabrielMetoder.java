09720fb5fa4c2377bd046007bb4d05a6c5809af8
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2022-12-20 15:13:22 +0100

    Har snart gjort klart gissningssystem, finns problem fortfarande

--- src/GabrielMetoder.java
+++ src/GabrielMetoder.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class GabrielMetoder {
     public static void main(String[] args) {
         // String name = "Gabriel"; (ordet som ska bli hemligt)
@@ -13,9 +15,40 @@ public class GabrielMetoder {
         }
         return maskedWord;
     }
-    public static String Guesses() {
-        String[] alfabet = {};
-        String[] guesses = {};
-        return;
+
+    public static String list() {
+        String guessedLetters = "";
+        return guessedLetters;
     }
+
+    public static boolean listContainsLetter(String list, char c) {
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == c) {
+                if (list.contains("" + c)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static String guessing(String y) {
+        int guesses = 0;
+        while (guesses < 15) {
+            String letter = JOptionPane.showInputDialog("Guess a letter");
+            char c = letter.charAt(0);
+            guesses++;
+            if (listContainsLetter(y,c)) {
+                int arrayNumber = 0;
+                char[] lettersGuessed = new char[26];
+                arrayNumber++;
+            }
+        }
+        return ;
+
+    }
+
 }
